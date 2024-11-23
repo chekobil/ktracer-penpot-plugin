2a5a94ee623138c9107cad9e8967337fb07803f6
kobino <felipe.arg@gmail.com>
2024-11-23 01:29:08 +0100

    plugin(): SVGs are appended to a flex board. Paths with stroke does not scale when shape.resize(), so the strokes ares scaled manually.

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,5 +1,5 @@
 import type { PluginMessageEvent } from "./types";
-import type { Shape } from "@penpot/plugin-types";
+import type { Shape, Board, FlexLayout } from "@penpot/plugin-types";
 
 const centerShape = (shape: Shape) => {
   shape.x = penpot.viewport.center.x;
@@ -21,7 +21,66 @@ penpot.ui.onMessage((message: PluginMessageEvent) => {
   } else if (message.type === "create-svg") {
     const svg = message.content;
     const shapeName = message.data.icon;
-    const shapeSize = message.data.size;
+    const shapeSize = Number(message.data.size);
+    const shapeScale = Number(message.data.scale);
+    let board: Board;
+    let flex: FlexLayout;
+    if (
+      penpot.selection?.length === 1 &&
+      penpot.selection[0]?.type === "board"
+    ) {
+      board = penpot.selection[0];
+    } else {
+      board = penpot.createBoard();
+      board.name = "icon-collection";
+      board.resize(300, 160);
+      centerShape(board);
+      flex = board.addFlexLayout();
+      flex.dir = "row";
+      flex.wrap = "wrap"; // not working
+      flex.alignItems = "center";
+      flex.alignContent = "start";
+      flex.justifyContent = "start";
+      flex.rowGap = 6;
+      flex.columnGap = 6;
+      flex.topPadding = 10;
+      flex.bottomPadding = 10;
+      flex.leftPadding = 10;
+      flex.rightPadding = 10;
+    }
+    if (board.flex) board.flex.wrap = "wrap"; // working
+    //console.log("FLEX::", flex);
+
+    selectShape(board);
+    // shapes.push(board);
+    const shape = penpot.createShapeFromSvg(svg);
+    if (shape) {
+      shape.name = shapeName;
+      shape.proportionLock = true;
+      shape.constraintsHorizontal = "scale";
+      shape.constraintsVertical = "scale";
+      shape.children.forEach((child) => {
+        // SCALE strokes, the same scale the shape is transformed from the viewBox
+        if (child.type === "path" && child.strokes?.length) {
+          const newStrokes: Shape["strokes"] = [];
+          const currentStrokes = [...child.strokes];
+          currentStrokes?.forEach((stroke) => {
+            if (stroke.strokeWidth) {
+              const newStroke = parseFloat(
+                Number(stroke.strokeWidth / shapeScale).toFixed(2)
+              );
+              stroke.strokeWidth = newStroke;
+            }
+            newStrokes.push(stroke);
+          });
+          child.strokes = [...newStrokes];
+          // console.log("SHAPE::", shapeScale, newStrokes, child.strokes);
+        }
+      });
+
+      board.appendChild(shape);
+      shape.resize(shapeSize, shapeSize);
+    }
   }
 });
 penpot.on("themechange", (theme) => {
